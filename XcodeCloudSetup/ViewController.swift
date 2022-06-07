9e40192692092966c1f0859caf9015b34194c708
SpeakapNik <nikhilesh.walde@speakap.nl>
2022-06-07 13:28:44 +0200

    Initial Commit

---
+++ XcodeCloudSetup/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  XcodeCloudSetup
+//
+//  Created by Nikhilesh Walde on 07/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
